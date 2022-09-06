e95e5297aa944639989223ea9f3e2ce2b3d59c13
Emiliano <emii88b@gmail.com>
2022-09-06 09:59:15 -0300

    git commit

--- App_ufree/urls.py
+++ App_ufree/urls.py
@@ -3,13 +3,13 @@ from App_ufree import views
 
 urlpatterns = [
     path('', views.inicio, name='Inicio'),
-    path("Job",views.Job),
-    path('Jobs', views.Jobs, name='MyProjects'),
-    path('Client', views.Client, name='Clientes'),
-    path('DateProject', views.DateProject, name = "DateProject"),
-    path('JobsForm', views.JobsForm, name = "JobsForm"),
-    path('ClientForm', views.ClienteForm, name = "ClientForm"),
-    path('search', views.search, name = "search"),
-    path('ClientSearch', views.ClientSearch, name="SearchClient"),
-    path('DeleteClient/<nombre_cliente>/', views.DeleteClient, name="EliminarCliente"),
+    path('/Job',views.Job),
+    path('/Jobs', views.Jobs, name='MyProjects'),
+    path('/Client', views.Client, name='Clientes'),
+    path('/DateProject', views.DateProject, name = "DateProject"),
+    path('/JobsForm', views.JobsForm, name = "JobsForm"),
+    path('/ClientForm', views.ClienteForm, name = "ClientForm"),
+    path('/search', views.search, name = "search"),
+    path('/ClientSearch', views.ClientSearch, name="SearchClient"),
+    path('/DeleteClient/<nombre_cliente>/', views.DeleteClient, name="EliminarCliente"),
     ]
